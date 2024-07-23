552b2c2e3b4a2720306007fb93cefe6b820add55
shubhIsSmart <shubham.kumar@flairminds.com>
2024-07-23 21:10:50 +0530

    Schema for user and category is added and fetched for testing

--- backend/index.ts
+++ backend/index.ts
@@ -1,13 +1,30 @@
-// import { PrismaClient } from '@prisma/client'
 const {PrismaClient} = require('@prisma/client')
 const prisma = new PrismaClient()
+const dotenv = require('dotenv')
 
+dotenv.config();
 async function main() {
   await prisma.$connect()
   console.log("Connected to database!")
 
+  const newUser = await prisma.user.create({
+    data: {
+      name: 'Shubham',
+      email: 'shubham460kumar@gmail.com',
+      password: 'geometry',
+      otp: '222222',
+      selectedCategories: {
+        create: {
+          name: 'Shoes',
+        },
+      },
+    },
+  })
+  console.log('Created new user: ', newUser)
+
   const allUsers = await prisma.user.findMany()
-  console.log(allUsers)
+  console.log('All users: ')
+  console.dir(allUsers, { depth: null })
 
   await prisma.$disconnect()
 }
